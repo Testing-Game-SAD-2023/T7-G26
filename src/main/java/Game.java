f8defc827ef09b685c20c0f0d2b16b737ef04d22
Pablo García Fernández <uo276824@uniovi.es>
2023-04-17 19:01:33 +0200

    update

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -32,34 +32,44 @@ public class Game {
     }
 
     public void startGame() throws Exception {
+        //Compile class
+        compileClass();
         //Ask if you want to compile the code
         Scanner sc = new Scanner(System.in);
         System.out.print("¿Do you want to compile the code? (y/n): ");
         String response = sc.next();
         if (response.equalsIgnoreCase("y")) {
-            boolean success = true;
-            if(success) {
-                //Compilation and return of an instance of the class
-                Class<?> clazz1 = Compilation.compileClass("model." + inputClassName,inputClassCode);
-                FileCreator.createFile(inputClassName,inputClassCode);
-                Class<?> clazz2 = Compilation.compileClass("model."+ inputTestClassName,inputTestClassCode);
-                //Ask if you want to execute the code
-                System.out.print("¿Do you want to execute the code? (y/n): ");
-                response = sc.next();
-                if (response.equalsIgnoreCase("y")) {
-                    //Run the test
-                    Execution.runTests(clazz2);
-                }else if (response.equalsIgnoreCase("n")) {
-                    System.out.println("No execution");
-                } else {
-                    System.out.println("Inavalid response");
-                }
-            }
+            compileTest(sc);
         } else if (response.equalsIgnoreCase("n")) {
             System.out.println("No compilation");
         } else {
             System.out.println("Inavalid response");
         }
         sc.close();
+    }
+
+    private void compileTest(Scanner sc) throws Exception {
+        boolean success = true;
+        String response;
+        if(success) {
+            //Compilation and return of an instance of the class
+            Class<?> clazz = Compilation.compileClass("model."+ inputTestClassName,inputTestClassCode);
+            //Ask if you want to execute the code
+            System.out.print("¿Do you want to execute the code? (y/n): ");
+            response = sc.next();
+            if (response.equalsIgnoreCase("y")) {
+                //Run the test
+                Execution.runTests(clazz);
+            }else if (response.equalsIgnoreCase("n")) {
+                System.out.println("No execution");
+            } else {
+                System.out.println("Inavalid response");
+            }
+        }
+    }
+
+    private void compileClass() throws Exception {
+        Compilation.compileClass("model." + inputClassName,inputClassCode);
+        FileCreator.createFile(inputClassName,inputClassCode);
     }
 }
