16a992363adc051e58f721c0dcf9a227bfa649ab
Pablo García Fernández <uo276824@uniovi.es>
2023-05-08 17:42:11 +0200

    Basic MVC display

---
+++ src/main/java/com/launcher/t7/T7Application.java
@@ -0,0 +1,13 @@
+package com.launcher.t7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class T7Application {
+    public static void main(String[] args) throws Exception {
+        //new Game("InputClass","InputTestClass").startGame();
+        SpringApplication.run(T7Application.class, args);
+    }
+
+}
