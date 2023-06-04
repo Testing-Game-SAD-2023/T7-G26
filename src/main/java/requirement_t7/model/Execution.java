6bbb56458d3156376b86787149f72c4264ca846d
Pablo García Fernández <uo276824@uniovi.es>
2023-06-04 20:01:41 +0200

    Save.

--- src/main/java/requirement_t7/model/Execution.java
+++ src/main/java/requirement_t7/model/Execution.java
@@ -4,18 +4,33 @@ import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
 import org.junit.runner.notification.Failure;
 
+import org.jacoco.agent.rt.RT;
+
 public class Execution {
     public static String runTests(Class<?> testClass) {
         String res = "";
-        // Run the JUnit tests
-        Result result = JUnitCore.runClasses(testClass);
 
-        // Print the results
-        for (Failure failure : result.getFailures()) {
-            res += failure.toString() + "\n";
+        try {
+            // Start the JaCoCo agent
+            RT.getAgent().dump(true);
+
+            // Run the JUnit tests
+            Result result = JUnitCore.runClasses(testClass);
+
+            // Print the results
+            for (Failure failure : result.getFailures()) {
+                res += failure.toString() + "\n";
+            }
+
+            res += "\n Tests run: " + result.getRunCount() + ", Failures: " + result.getFailureCount() + ", Ignored: " + result.getIgnoreCount();
+
+            // Stop the JaCoCo agent and collect coverage information
+            RT.getAgent().dump(true);
+        } catch (Exception e) {
+            // Handle any exceptions that occur during test execution or coverage collection
+            res = "Exception occurred: " + e.getMessage();
         }
 
-        res += "\n Tests run: " + result.getRunCount() + ", Failures: " + result.getFailureCount() + ", Ignored: " + result.getIgnoreCount();
         return res;
     }
 }
